6127a323b7c8f249ecf818edb123a93b5dda47c7
Varoto7 <szymonej9@gmail.com>
2026-04-01 11:23:20 +0200

    ✅

---
+++ lab_06/utils.py
@@ -0,0 +1,25 @@
+from config import SCREEN_W, SCREEN_H
+
+def ghost_positions(x, y, size):
+    positions = [(x, y)]
+
+    wrap_x = None
+    if x < size:
+        wrap_x = x + SCREEN_W
+    elif x > SCREEN_W - size:
+        wrap_x = x - SCREEN_W
+
+    wrap_y = None
+    if y < size:
+        wrap_y = y + SCREEN_H
+    elif y > SCREEN_H - size:
+        wrap_y = y - SCREEN_H
+
+    if wrap_x is not None:
+        positions.append((wrap_x, y))
+    if wrap_y is not None:
+        positions.append((x, wrap_y))
+    if wrap_x is not None and wrap_y is not None:
+        positions.append((wrap_x, wrap_y))
+
+    return positions
